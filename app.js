0d7bf017ad1529ea9d8aa7e1f809b278ab7a6e54
mvang <mvang@yangs-iMac.local>
2019-04-02 11:37:56 -0700

    completed greetings, main and hello-gif.handlebars.

--- app.js
+++ app.js
@@ -14,4 +14,18 @@ app.get('/hello-squirrel', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
- 
+
+var exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/hello-gif', function (req, res) {
+  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+  res.render('hello-gif', {gifUrl: gifUrl})
+})
+
+app.get('/greetings/:name', function (req, res) {
+  var name = req.params.name;
+  res.render('greetings', {name: name});
+})
